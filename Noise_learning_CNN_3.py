ccb9c4535aaf97df4d68f382439aa1f4975d5969
Fred-23 <frederic.Alphonse@eleves.ec-nantes.fr>
2022-03-22 13:00:57 +0100

    Fonctionnel

---
+++ Noise_learning_CNN_3.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from PIL import Image, ImageOps, ImageFilter
+
+# Charger l'image sous forme d'une matrice de pixels
+img = np.array(Image.open('simba.png'))
+
+# Générer le bruit gaussien de moyenne nulle et d'écart-type 7 (variance 49)
+noise = np.random.normal(0, 7, img.shape)
+
+# Créer l'image bruitée et l'afficher
+noisy_img = Image.fromarray(img + noise).convert('L')
+noisy_img.show()
+
+# Appliquer le lissage par moyennage (fenêtre de taille 9) et afficher le résultat
+noisy_img.filter(ImageFilter.BoxBlur(1)).show()
+
+##Test des filtres pour rajouter du bruit/blur
