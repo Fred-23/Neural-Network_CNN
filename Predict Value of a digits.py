f949582042dc21ebe176ef82b4ca4f378a0ac624
Fred-23 <frederic.Alphonse@eleves.ec-nantes.fr>
2022-04-25 21:57:33 +0200

    Add samples to test the CNN

--- Predict Value of a digits.py
+++ Predict Value of a digits.py
@@ -1,15 +1,20 @@
 # make a prediction for a new image.
-from numpy import argmax
+import numpy as geek
+from numpy import argmax, invert
+from tensorflow.keras.optimizers import SGD
 from keras.preprocessing.image import load_img
 from keras.preprocessing.image import img_to_array
 from keras.models import load_model
 
 # load and prepare the image
+
+
 def load_image(filename):
 	# load the image
 	img = load_img(filename, grayscale=True, target_size=(28, 28))
 	# convert to array
 	img = img_to_array(img)
+	#img = geek.invert(img)
 	# reshape into a single sample with 1 channel
 	img = img.reshape(1, 28, 28, 1)
 	# prepare pixel data
@@ -18,13 +23,16 @@ def load_image(filename):
 	return img
 
 # load an image and predict the class
+
+
 def run_example():
 	# load the image
-	img = load_image('2_keras.png')
+	img = load_image('six_5.png')
 	# load model
 	model = load_model('final_model.h5')
 	# predict the class
 	predict_value = model.predict(img)
+	
 	digit = argmax(predict_value)
 	print(digit)
 
