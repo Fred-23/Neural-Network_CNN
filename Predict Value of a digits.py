e4f535d811133258ac72290177803ecdf06d57fc
Fred-23 <frederic.Alphonse@eleves.ec-nantes.fr>
2022-04-27 21:34:20 +0200

    Reorganization of the project

--- Predict Value of a digits.py
+++ Predict Value of a digits.py
@@ -27,7 +27,7 @@ def load_image(filename):
 
 def run_example():
 	# load the image
-	img = load_image('six_5.png')
+	img = load_image('Images_Samples/six_5.png')
 	# load model
 	model = load_model('final_model.h5')
 	# predict the class
